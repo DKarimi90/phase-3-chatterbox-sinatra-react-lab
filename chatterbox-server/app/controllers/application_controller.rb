c8767efe68630d3f8f168da13f6781be510e9d01
david <davmwa90@gmail.com>
2023-03-01 08:09:00 +0300

    completed the chatterbox lab

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -3,4 +3,33 @@ class ApplicationController < Sinatra::Base
   
   # Add routes
   
+  get '/messages' do 
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json 
+  end
+
+
+  post '/messages' do 
+    post = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    post.to_json 
+  end
+
+  patch '/messages/:id' do 
+    message_body = Message.find(params[:id])
+    message_body.update(
+      body: params[:body], 
+      username: params[:username]
+    )
+    message_body.to_json
+  end
+
+
+  delete '/messages/:id' do 
+    message = Message.find(params[:id])
+    message.destroy 
+  end
+
 end
